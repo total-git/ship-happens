5723c92bdab87644defb36bf1163e83b026a1def
Hendrik v. Raven <hendrik@consetetur.de>
2015-01-30 17:14:01 +0100

    BoardTest: more Testcases

--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -80,5 +80,47 @@ class BoardTest extends FlatSpec with Matchers {
     b.setShip(Ship("Testship", h-3), (3,0), Orientation.Vertical) should not be empty
   }
 
+  "A ship at length 3 vertical ship from B1 to B3" should "be have 3 lives at beginning" in {
+    b.shipAt("B1") should not be empty
+    b.shipAt("B1").get.lives shouldBe 3
+  }
+
+  it should "return the same ship on all three positions" in {
+    b.shipAt("B1") shouldBe b.shipAt("B2")
+    b.shipAt("B1") shouldBe b.shipAt("B3")
+  }
+
+  it should "be unprobed" in {
+    b.isProbed("B1") shouldBe false
+    b.isProbed("B2") shouldBe false
+    b.isProbed("B3") shouldBe false
+  }
+
+  it should "be have 2 lives after bombing B1" in {
+    val (b1,r1) = b.probeSquare("B1")
+    r1 shouldBe Result.Hit
+    b1.shipAt("B1").get.lives shouldBe 2
+  }
+
+  it should "be sunk after three hits" in {
+    val (b1,r1) = b.probeSquare("B1")
+    val (b2,r2) = b1.probeSquare("B2")
+    val (b3,r3) = b2.probeSquare("B3")
+    b1.shipAt("B1").get.lives shouldBe 2
+    r1 shouldBe Result.Hit
+    b2.shipAt("B1").get.lives shouldBe 1
+    r2 shouldBe Result.Hit
+    b3.shipAt("B1").get.lives shouldBe 0
+    r3 shouldBe Result.Sunk
+  }
+
+  it should "not change something to hit the same field twice" in {
+    val (b1,r1) = b.probeSquare("B1")
+    val (b2,r2) = b1.probeSquare("B1")
+    b2.shipAt("B1").get.lives shouldBe b1.shipAt("B1").get.lives
+    r2 shouldBe Result.AlreadyProbed
+  }
+
+
 }
 
