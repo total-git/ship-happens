b3046ebdb9dd8d1745af380881ea123704018d73
Felix Reihl <felix.reihl@gmail.com>
2015-02-06 18:02:58 +0100

    Catch wrongly formatted input strings using regex

--- src/main/scala/shiphappens/Game.scala
+++ src/main/scala/shiphappens/Game.scala
@@ -3,14 +3,17 @@ package shiphappens.Game
 import shiphappens.Board._
 
 import shiphappens.Player._
+import shiphappens.Types._
 
 object Game {
   // Just for testing
   def main(args: Array[String]) {
     var b = new Board()
     var p = new HumanPlayer(0)
-    //p.bomb(b.visible)
-    p.printEnemy(b.visible)
-    p.printOwn(b.full)
+    p.bomb(b.visible)
+    //p.printEnemy(b.visible)
+    //p.printOwn(b.full)
+    //val s = new Ship("Battleship", 5)
+    //p.placeShip(s, b.full)
   }
 }
--- src/main/scala/shiphappens/Player.scala
+++ src/main/scala/shiphappens/Player.scala
@@ -1,5 +1,7 @@
 package shiphappens.Player
 
+import scala.util.matching.Regex
+
 // import bloat
 import shiphappens.Board._
 import shiphappens.Board.Board._
@@ -21,18 +23,35 @@ trait Player {
 
 case class HumanPlayer(val id: Int)  extends Player {
   def bomb(field: Board.EnemyField): Coordinates = {
-    // TODO handle wrong formatting
-    print("Shoot which square? (A to %s, 1 to %d, e.g. A4): ".format((field.size+'A').toChar.toString, field(0).size))
-    val coord : Coordinates = new Coordinates(readLine())
+    var buffer = ""
+    while(!buffer.matches("\\A[A-Z]\\d+\\s*\\z")) {
+      buffer = readLine("Shoot which square? (A to %s, 1 to %d, e.g. A4): ".format((field.size+'A').toChar.toString, field(0).size))
+    }
+    val coord : Coordinates = new Coordinates(buffer)
     return coord
   }
   def placeShip(ship: Ship, field: Board.PlayerField): (Coordinates,Orientation) = {
-    ???
+    var buffer = ""
+    while(!buffer.matches("\\A[A-Z]\\d+\\s*\\z")) {
+      buffer = readLine("Place %s (%d long) where? Give coordinates of the top left square: ".format(ship.name,ship.length))
+    }
+    val coord : Coordinates = new Coordinates(buffer)
+    while(!(buffer.matches("\\A[hv]\\s*\\z"))) {
+      buffer = readLine("And the orientation (h|v) for horizontal or vertical: ")
+    }
+    val orientation = buffer match {
+      case "h" => Horizontal
+      case "v" => Vertical
+      case _   => Vertical
+    }
+    return (coord, orientation)
   }
+
   def update(ownField: Board.PlayerField, enemyField: Board.EnemyField,
              player: Player, move: Move) = {
     ???
   }
+
   def printEnemy(f: Board.EnemyField) {
     var rowCount : Int = 0
     val validChars = ('A' to 'Z')
