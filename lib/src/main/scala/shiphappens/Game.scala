af86653260bb4cc935faf6e2c8d04a350cb2f447
Hendrik v. Raven <hendrik@consetetur.de>
2015-02-09 00:03:12 +0100

    rowerked Game

--- lib/src/main/scala/shiphappens/Game.scala
+++ lib/src/main/scala/shiphappens/Game.scala
@@ -5,64 +5,95 @@ import shiphappens.Board._
 import shiphappens.Player._
 import shiphappens.Types._
 import shiphappens.Types.Coordinates._
+import shiphappens.Types.PlayerId.{Self,Enemy}
 import shiphappens.Types.Orientation._
 import shiphappens.Types.Result._
 
+case class PlayerStruct(var player: Player,
+                        var board: Board,
+                        var ships: List[Ship])
 
 object Game {
-  val ships : Array[Ship] = Array(Ship("Battleship", 5))//, Ship("Cruiser", 4), Ship("Cruiser", 4), Ship("Destroyer", 3), Ship("Destroyer", 3), Ship("Destroyer", 3), Ship("Submarine", 2), Ship("Submarine", 2), Ship("Submarine", 2), Ship("Submarine", 2))
+  val startShips : List[Ship] = List(Ship("Battleship", 5), Ship("Cruiser", 4), Ship("Cruiser", 4), Ship("Destroyer", 3), Ship("Destroyer", 3), Ship("Destroyer", 3), Ship("Submarine", 2), Ship("Submarine", 2), Ship("Submarine", 2), Ship("Submarine", 2))
+}
 
-  // request a placing from the player until it is valid (not colliding or adjacent to already placed ships)
-  def getCorrectPlacing(player: Player, enemyField: Board.EnemyField, ownBoard: Board, ship: Ship) : (Board) = {
-    val (tmpCoord: Coordinates, tmpOrient: Orientation) = player.requestPlacing(ownBoard.full, ship)
-    val tmpBoard: Option[Board] = ownBoard.setShip(ship, tmpCoord, tmpOrient)
-    println(tmpBoard)
-    println(tmpCoord, tmpOrient)
-    tmpBoard match {
-      case None        => return getCorrectPlacing(player, enemyField, ownBoard, ship)
-      case Some(b)     => {
-        player.update(b.full, enemyField, new Placed(ship, tmpOrient, tmpCoord))
-        return b
+class Game(players: (Player, Player)) {
+  private var _players: (PlayerStruct,PlayerStruct)
+    = (PlayerStruct(players._1, new Board(), Game.startShips),
+       PlayerStruct(players._2, new Board(), Game.startShips))
+  private var next: Int = 1
+
+  private def get(id: Int) = id match {
+    case 1 => _players._1
+    case 2 => _players._2
+  }
+
+  private def enemy(id: Int) = id match {
+    case 1 => _players._2
+    case 2 => _players._1
+  }
+
+  // places a ship at the given position
+  def placeShip(id: Int, ship: Ship, coords: Coordinates,
+                orient: Orientation): Boolean = {
+    var p = get(id)
+    if (p.ships.isEmpty)
+      return false
+
+    if (ship == p.ships.head) {
+      p.board.setShip(ship, coords, orient) match {
+        case None    => return false;
+        case Some(b) => {
+          // valid move, update board & remove ship from List
+          p.board = b
+          p.ships = p.ships.tail
+          p.player.update(p.board.full, enemy(id).board.visible,
+                          Placed(ship, orient, coords))
+          if (!p.ships.isEmpty) {
+            p.player.requestPlacing(p.board.full, p.ships.head)
+          } else if (enemy(id).ships.isEmpty) {
+            // all ships are placed now notify player 1 to make the first
+            // move
+            get(1).player.requestShot(get(2).board.visible)
+          }
+          return true
+        }
       }
+    } else {
+      // ship is not first in list, player tries to set something else
+      return false
     }
   }
 
-  // call getCorrectPlacing for all ships
-  def placeShips(player: Player, ownBoard: Board, enemyField: Board.EnemyField) : Board= {
-    ships.foldLeft(ownBoard)(getCorrectPlacing(player, enemyField, _: Board, _: Ship))
+  def makeMove(id: Int, coords: Coordinates): Boolean = {
+    // make sure it's the players turn
+    if (id != next)
+      return false
+
+    val p = get(id)
+    val e = enemy(id)
+    val (b,r) = e.board.probeSquare(coords)
+    e.board = b
+    p.player.update(p.board.full, e.board.visible,
+                    Shot(Self, coords, r))
+    e.player.update(e.board.full, p.board.visible,
+                    Shot(Enemy, coords, r))
+
+    // if the game is not over notify the other player that it's his turn
+    if (r != Won) {
+      e.player.requestShot(p.board.visible)
+      next = (next+1)%2
+    } else {
+      // we set this to 0, meaning nobody is allowed to make moves
+      next = 0
+    }
+    return true
   }
 
-  // lets the player shoot until he misses or enters an invalid coordinate
-  def shootLoop(ownPlayer: Player, enemyPlayer: Player, ownBoard: Board, enemyBoard: Board) : Boolean = {
-    val coord : Coordinates = ownPlayer.requestShot(enemyBoard.visible)
-    val probeResult = enemyBoard.probeSquare(coord)
-    // update the player
-    probeResult match {
-      case (b, result) => {
-        ownPlayer.update(ownBoard.full, b.visible, new Shot(ownPlayer, coord, result))
-        enemyPlayer.update(b.full, ownBoard.visible, new Shot(ownPlayer, coord, result))
-      }
-    }
-    // re-call the function if a ship was hit, let the other player shoot if not
-    probeResult match {
-      case (b, Sunk) => return shootLoop(ownPlayer, enemyPlayer, ownBoard, b)
-      case (b, Hit) => return shootLoop(ownPlayer, enemyPlayer, ownBoard, b)
-      // let the enemy shoot
-      case (b, Miss) => return shootLoop(enemyPlayer, ownPlayer, b, ownBoard)
-      case (b, Invalid) => return shootLoop(enemyPlayer, ownPlayer, b, ownBoard)
-      case (b, AlreadyProbed) => return shootLoop(enemyPlayer, ownPlayer, b, ownBoard)
-      case (_, Won) => return true
-    }
+  def start = {
+    // first place ships
+    get(1).player.requestPlacing(get(1).board.full, get(1).ships.head)
+    get(2).player.requestPlacing(get(2).board.full, get(2).ships.head)
   }
 
-  // Just for testing
-  def main(args: Array[String]) {
-    var b1 = new Board()
-    var b2 = new Board()
-    var p1 = new HumanPlayer(1)
-    var p2 = new HumanPlayer(2)
-    val b1p = placeShips(p1, b1, b2.visible)
-    val b2p = placeShips(p2, b2, b1.visible)
-    shootLoop(p1, p2, b1p, b2p)
-  }
 }
