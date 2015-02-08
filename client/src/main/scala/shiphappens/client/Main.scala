9f987ecd0856e56f176686edf6ebbcb312e418ed
Felix Reihl <felix.reihl@gmail.com>
2015-02-08 23:08:06 +0100

    some playing around with the client player

--- client/src/main/scala/shiphappens/client/Main.scala
+++ client/src/main/scala/shiphappens/client/Main.scala
@@ -1,13 +1,17 @@
 package shiphappens.Client
 
-import shiphappens.Player.HumanPlayer
+import shiphappens.Client._
+import shiphappens.Types._
+import shiphappens.Types.Coordinates._
+import shiphappens.Types.Orientation._
 
 object client {
   def main(args: Array[String]) {
-    val p = new Player(1)
-    val h = new HumanPlayer(1)
-    val boards = p.getOwnBoard
-    h.printOwn(boards)
-//    h.printEnemy(boards._2)
+    //val p = new Player(1)
+    //val h = new HumanPlayer(1)
+    //val boards = p.getOwnBoard
+    //h.printOwn(boards)
+    //h.printEnemy(boards._2)
+    print("foo")
   }
 }
--- client/src/main/scala/shiphappens/client/Player.scala
+++ client/src/main/scala/shiphappens/client/Player.scala
@@ -5,14 +5,46 @@ import uk.co.bigbeeconsultants.http.response.Response
 import java.net.URL
 
 import shiphappens.Board._
+import shiphappens.Types._
 import shiphappens.Types.Result._
+import shiphappens.Types.Coordinates._
+import shiphappens.Types.Orientation._
 
 class Player(val id: Int) {
   private val client = new HttpClient
+  var state : String = ""
 
   def getBoards(): String = {
     val response = client.get(new URL("http://localhost:9000/api/get/" + id.toString))
-    response.body.asString
+      response match {
+        case _:Unit =>
+        case r => state = r.body.asString
+      }
   }
 
+  def shoot() : Coordinates = {
+    var buffer : String = ""
+    // only checks if the coordinates can be parsed, not if they are inside the boundaries or already bombed
+    while(!buffer.matches("\\A[A-Z]\\d+\\s*\\z")) {
+      buffer = readLine("Shoot which square? (e.g. A4): ")
+    }
+    val coord : Coordinates = new Coordinates(buffer)
+    return coord
+  }
+
+  def place(ship: Ship) : (Coordinates,Orientation) = {
+    var buffer : String = ""
+    while(!buffer.matches("\\A[A-Z]\\d+\\s*\\z")) {
+      buffer = readLine("Place %s where? Give coordinates of the top left square: ".format(ship))
+    }
+    val coord : Coordinates = new Coordinates(buffer)
+    while(!(buffer.matches("\\A[hv]\\s*\\z"))) {
+      buffer = readLine("And the orientation (h|v) for horizontal or vertical: ")
+    }
+    val orientation : Orientation = buffer match {
+      case "h" => Horizontal
+      case "v" => Vertical
+    }
+    return (coord, orientation)
+  }
 }
