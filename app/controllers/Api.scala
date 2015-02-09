ac83f596992137ce79dcd6d37e92e2faf7aeac15
Hendrik v. Raven <hendrik@consetetur.de>
2015-02-09 01:07:35 +0100

    Api: implemented place

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -7,6 +7,8 @@ import play.api.mvc.Results._
 import play.api.libs.json._
 import play.api.libs.json.Json._
 
+import scala.util.matching.Regex
+
 import models.PlayGame
 import shiphappens.Types._
 import shiphappens.Board._
@@ -51,12 +53,17 @@ object Api extends Controller {
   def place(id: Int) = Action(parse.tolerantText) { request =>
     if (id < 1 || id > 2)
       BadRequest("Player ID invalid, only two players can play ship-happens")
-    // parse some data
-    val coords = (0,0)
-    val orient = Orientation.Horizontal
-    PlayGame.player(id).placeShip(coords,orient) match {
-      case true => Ok("Ship placed succesfully")
-      case false => BadRequest("Failed to place ship")
+    else if (!request.body.matches("""\w+\d+ (Horizontal|Vertical)"""))
+      BadRequest("Input is invalid. Not a valid placement specification")
+    else {
+      val s = request.body.split(" ")
+      val coords = s(0)
+      val orient = Orientation.withName(s(1))
+
+      PlayGame.player(id).placeShip(coords,orient) match {
+        case true => Ok("Ship placed succesfully")
+        case false => BadRequest("Failed to place ship")
+      }
     }
   }
 }
